f1c711c4ff613c62902c131efb355e5ea26f1519
atharoxia <havas.attila@szbi-pg.hu>
2025-10-08 08:40:59 +0200

    Update szamkitalalo.py

--- listak.py
+++ listak.py
@@ -16,4 +16,27 @@ print(tanuloiq)
 reszlista = tanuloiq[5:10]  #Az első szám a kezdőindex, a második eggyel nagyobb, mint az utolsó
 print(reszlista)
 
+#Mennyi az osztály IQ-jának átlaga két tizedes pontossággal:
+osszeg = 0
+for i in range(32):
+    osszeg += tanuloiq[i]
 
+atlag = round(osszeg / 32, 2)
+print(f"Az osztály IQ-jának átlaga: {atlag}")
+
+#Hány tanuló IQ-ja 130 feletti:
+db = 0
+for i in range(32):
+    if tanuloiq[i] > 130:
+        db += 1
+print(f"{db} tanulónak nagyobb 130-nál az IQ-ja.")
+
+#Hányadik a legokosabb tanuló, és mennyi az IQ-ja:
+
+max = 0
+maxi = 0
+for i in range(32):
+    if tanuloiq[i] > max:
+        max = tanuloiq[i]
+        maxi = i
+print(f"A legokosabb a {maxi + 1}-edik tanuló, {max} az IQ-ja.")
